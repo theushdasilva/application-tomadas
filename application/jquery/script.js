1186d20309723e4d59c6815f362efd284a72542a
theushdasilva <39439666+theushdasilva@users.noreply.github.com>
2021-01-06 15:28:35 -0300

    Add files via upload

---
+++ application/jquery/script.js
@@ -0,0 +1,268 @@
+
+(function(){/*encapsulando fuçãos*/
+
+	function active(){ /*Criando Função*/
+
+			$(".ref").hover(function(){
+		  		$(this).toggleClass("active");
+			});
+	}
+	 active();/*Ativando Função "menu"*/
+}());
+
+
+//numeros das cores
+(function(){function number(){$(".c1").click(function(){$(".number").html("<b>1</b>");});}number();}());
+(function(){function number(){$(".c2").click(function(){$(".number").html("<b>2</b>");});}number();}());
+(function(){function number(){$(".c3").click(function(){$(".number").html("<b>3</b>");});}number();}());
+(function(){function number(){$(".c4").click(function(){$(".number").html("<b>4</b>");});}number();}());
+(function(){function number(){$(".c5").click(function(){$(".number").html("<b>5</b>");});}number();}());
+(function(){function number(){$(".c6").click(function(){$(".number").html("<b>6</b>");});}number();}());
+(function(){function number(){$(".c7").click(function(){$(".number").html("<b>7</b>");});}number();}());
+(function(){function number(){$(".c8").click(function(){$(".number").html("<b>8</b>");});}number();}());
+(function(){function number(){$(".c9").click(function(){$(".number").html("<b>9</b>");});}number();}());
+(function(){function number(){$(".c10").click(function(){$(".number").html("<b>10</b>");});}number();}());
+(function(){function number(){$(".c11").click(function(){$(".number").html("<b>11</b>");});}number();}());
+(function(){function number(){$(".c12").click(function(){$(".number").html("<b>12</b>");});}number();}());
+(function(){function number(){$(".c13").click(function(){$(".number").html("<b>13</b>");});}number();}());
+(function(){function number(){$(".c14").click(function(){$(".number").html("<b>14</b>");});}number();}());
+(function(){function number(){$(".c15").click(function(){$(".number").html("<b>15</b>");});}number();}());
+(function(){function number(){$(".c16").click(function(){$(".number").html("<b>16</b>");});}number();}());
+(function(){function number(){$(".c17").click(function(){$(".number").html("<b>17</b>");});}number();}());
+(function(){function number(){$(".c18").click(function(){$(".number").html("<b>18</b>");});}number();}());
+(function(){function number(){$(".c19").click(function(){$(".number").html("<b>19</b>");});}number();}());
+(function(){function number(){$(".c20").click(function(){$(".number").html("<b>20</b>");});}number();}());
+(function(){function number(){$(".c21").click(function(){$(".number").html("<b>21</b>");});}number();}());
+(function(){function number(){$(".c22").click(function(){$(".number").html("<b>22</b>");});}number();}());
+(function(){function number(){$(".c23").click(function(){$(".number").html("<b>23</b>");});}number();}());
+(function(){function number(){$(".c24").click(function(){$(".number").html("<b>24</b>");});}number();}());
+(function(){function number(){$(".c25").click(function(){$(".number").html("<b>25</b>");});}number();}());
+(function(){function number(){$(".c26").click(function(){$(".number").html("<b>26</b>");});}number();}());
+(function(){function number(){$(".c27").click(function(){$(".number").html("<b>27</b>");});}number();}());
+(function(){function number(){$(".c28").click(function(){$(".number").html("<b>28</b>");});}number();}());
+(function(){function number(){$(".c29").click(function(){$(".number").html("<b>29</b>");});}number();}());
+(function(){function number(){$(".c30").click(function(){$(".number").html("<b>30</b>");});}number();}());
+
+//numeros das texturas
+(function(){function number(){$(".textura1").click(function(){$(".number_2").html("<b>1</b>");});}number();}());
+(function(){function number(){$(".textura2").click(function(){$(".number_2").html("<b>2</b>");});}number();}());
+(function(){function number(){$(".textura3").click(function(){$(".number_2").html("<b>3</b>");});}number();}());
+(function(){function number(){$(".textura4").click(function(){$(".number_2").html("<b>4</b>");});}number();}());
+(function(){function number(){$(".textura5").click(function(){$(".number_2").html("<b>5</b>");});}number();}());
+(function(){function number(){$(".textura6").click(function(){$(".number_2").html("<b>6</b>");});}number();}());
+(function(){function number(){$(".textura7").click(function(){$(".number_2").html("<b>7</b>");});}number();}());
+(function(){function number(){$(".textura8").click(function(){$(".number_2").html("<b>8</b>");});}number();}());
+(function(){function number(){$(".textura9").click(function(){$(".number_2").html("<b>9</b>");});}number();}());
+(function(){function number(){$(".textura10").click(function(){$(".number_2").html("<b>10</b>");});}number();}());
+(function(){function number(){$(".textura11").click(function(){$(".number_2").html("<b>11</b>");});}number();}());
+
+
+
+/*Trocando as Imagens Tomada"*/
+function img_01(){
+	document.getElementById("trocarimgtomada").src ="img/tomadas/img_01.png"
+}
+
+function img_02(){
+	document.getElementById("trocarimgtomada").src ="img/tomadas/img_02.png"
+}
+
+function img_03(){
+	document.getElementById("trocarimgtomada").src ="img/tomadas/img_03.png"
+}
+
+function img_04(){
+	document.getElementById("trocarimgtomada").src ="img/tomadas/img_04.png"
+}
+
+function img_05(){
+	document.getElementById("trocarimgtomada").src ="img/tomadas/img_05.png"
+}
+
+function img_06(){
+	document.getElementById("trocarimgtomada").src ="img/tomadas/img_06.png"
+}
+
+/*Trocando as textura Parede"*/
+
+function img_parede_01(){
+	document.getElementById("trocarimgparede").src ="img/parede/img_01.jpg"
+}
+
+function img_parede_02(){
+	document.getElementById("trocarimgparede").src ="img/parede/img_02.jpg"
+}
+
+function img_parede_03(){
+	document.getElementById("trocarimgparede").src ="img/parede/img_03.jpg"
+}
+
+function img_parede_04(){
+	document.getElementById("trocarimgparede").src ="img/parede/img_04.jpg"
+}
+
+function img_parede_05(){
+	document.getElementById("trocarimgparede").src ="img/parede/img_05.jpg"
+}
+
+function img_parede_06(){
+	document.getElementById("trocarimgparede").src ="img/parede/img_06.jpg"
+}
+
+function img_parede_07(){
+	document.getElementById("trocarimgparede").src ="img/parede/img_07.jpg"
+}
+
+function img_parede_08(){
+	document.getElementById("trocarimgparede").src ="img/parede/img_08.jpg"
+}
+
+function img_parede_09(){
+	document.getElementById("trocarimgparede").src ="img/parede/img_09.jpg"
+}
+
+function img_parede_10(){
+	document.getElementById("trocarimgparede").src ="img/parede/img_10.jpg"
+}
+
+function img_parede_11(){
+	document.getElementById("trocarimgparede").src ="img/parede/img_11.jpg"
+}
+
+/*Trocando cor da parede*/
+
+function cor_parede_01(){
+	document.getElementById("imagem_parede").style.backgroundColor = "rgb(169,169,169)";
+}
+
+function cor_parede_02(){
+	document.getElementById("imagem_parede").style.backgroundColor = "rgb(79,79,79)";
+}
+
+function cor_parede_03(){
+	document.getElementById("imagem_parede").style.backgroundColor = "rgb(131,111,255)";
+}
+
+function cor_parede_04(){
+	document.getElementById("imagem_parede").style.backgroundColor = "rgb(72,61,139)";
+}
+
+function cor_parede_05(){
+	document.getElementById("imagem_parede").style.backgroundColor = "rgb(0,0,255)";
+}
+
+function cor_parede_06(){
+	document.getElementById("imagem_parede").style.backgroundColor = "rgb(100,149,237)";
+}
+
+function cor_parede_07(){
+	document.getElementById("imagem_parede").style.backgroundColor = "rgb(65,105,225)";
+}
+
+function cor_parede_08(){
+	document.getElementById("imagem_parede").style.backgroundColor = "rgb(30,144,255)";
+}
+
+function cor_parede_09(){
+	document.getElementById("imagem_parede").style.backgroundColor = "rgb(0,191,255)";
+}
+
+function cor_parede_10(){
+	document.getElementById("imagem_parede").style.backgroundColor = "rgb(135,206,235)";
+}
+
+function cor_parede_11(){
+	document.getElementById("imagem_parede").style.backgroundColor = "rgb(70,130,180)";
+}
+
+function cor_parede_12(){
+	document.getElementById("imagem_parede").style.backgroundColor = "rgb(176,196,222)";
+}
+
+function cor_parede_13(){
+	document.getElementById("imagem_parede").style.backgroundColor = "rgb(0,255,255)";
+}
+
+function cor_parede_14(){
+	document.getElementById("imagem_parede").style.backgroundColor = "rgb(64,224,208)";
+}
+
+function cor_parede_15(){
+	document.getElementById("imagem_parede").style.backgroundColor = "rgb(72,209,204)";
+}
+
+function cor_parede_16(){
+	document.getElementById("imagem_parede").style.backgroundColor = "rgb(32,178,170)";
+}
+
+function cor_parede_17(){
+	document.getElementById("imagem_parede").style.backgroundColor = "rgb(127,255,212)";
+}
+
+function cor_parede_18(){
+	document.getElementById("imagem_parede").style.backgroundColor = "rgb(102,205,170)";
+}
+
+function cor_parede_19(){
+	document.getElementById("imagem_parede").style.backgroundColor = "rgb(107,142,35)";
+}
+
+function cor_parede_20(){
+	document.getElementById("imagem_parede").style.backgroundColor = "rgb(85,107,47)";
+}
+
+function cor_parede_21(){
+	document.getElementById("imagem_parede").style.backgroundColor = "rgb(128,128,0)";
+}
+
+function cor_parede_22(){
+	document.getElementById("imagem_parede").style.backgroundColor = "rgb(218,165,32)";
+}
+
+function cor_parede_23(){
+	document.getElementById("imagem_parede").style.backgroundColor = "rgb(184,134,11)";
+}
+
+function cor_parede_24(){
+	document.getElementById("imagem_parede").style.backgroundColor = "rgb(255,182,193)";
+}
+
+function cor_parede_25(){
+	document.getElementById("imagem_parede").style.backgroundColor = "rgb(255,192,203)";
+}
+
+function cor_parede_26(){
+	document.getElementById("imagem_parede").style.backgroundColor = "rgb(240,128,128)";
+}
+
+function cor_parede_27(){
+	document.getElementById("imagem_parede").style.backgroundColor = "rgb(205,92,92)";
+}
+
+function cor_parede_28(){
+	document.getElementById("imagem_parede").style.backgroundColor = "rgb(255,160,122)";
+}
+
+function cor_parede_29(){
+	document.getElementById("imagem_parede").style.backgroundColor = "rgb(255,127,80)";
+}
+
+function cor_parede_30(){
+	document.getElementById("imagem_parede").style.backgroundColor = "rgb(255,99,71)";
+}
+
+function next_textura(){
+	document.getElementById("box_03_textura").scrollBy(900, 0);
+}
+
+function prev_textura(){
+	document.getElementById("box_03_textura").scrollBy(-900, 0);
+}
+
+function next_cor(){
+	document.getElementById("box_03").scrollBy(900, 0);
+}
+
+function prev_cor(){
+	document.getElementById("box_03").scrollBy(-900, 0);
+}
